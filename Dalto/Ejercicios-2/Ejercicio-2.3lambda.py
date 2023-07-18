3598835123f5ac2bfe4c74fe3d59be8a1d28744b
yazbek1989 <yazbek1989@gmail.com>
2023-07-18 07:52:41 -0600

    modified:   Dalto/Ejercicios-2/Ejercicio-2.3lambda.py 	deleted:    Dalto/Ejercicios-2/Ejercicio-2.1.py 	deleted:    Dalto/Ejercicios-2/Ejercicio-2.2.py 	deleted:    Dalto/Ejercicios-2/Ejercicio-2.3.py
    	Dalto/Ejercicios-2/Ejercicio-2.4.py
    	Dalto/Ejercicios-2/Ejercicio-2.5.py
    	Dalto/Ejercicios-2/Ejercicio-2.6.py
    	Dalto/Ejercicios-2/Fibonacci.py
    	Dalto/Ejercicios-2/Primocontar.py
    	Dalto/Ejercicios-2/asistencia.py

--- Dalto/Ejercicios-2/Ejercicio-2.3lambda.py
+++ Dalto/Ejercicios-2/Ejercicio-2.3lambda.py
@@ -1,4 +1,5 @@
 #contar primos hasta x numero e imprimirlos, con dos funciones lambda anidadas
+#from Primocontar import primoshasta as contar_primo
 primos_hasta = lambda num: list(filter(lambda x: all(x % i != 0 for i in range(2, int(x ** 0.5)+1)), range(2, num+1)))
 #imprimir primos
 print(primos_hasta(13))
